caa35e0cf283fbd5a3688c994a5a0c248fbf37f4
SmDum <samueldemoraesdelgado2005@gmail.com>
2025-01-03 17:30:00 -0300

    Exercícios

--- src/componentes/Pesquisa/index.js
+++ src/componentes/Pesquisa/index.js
@@ -1,34 +1,51 @@
 import Input from '../Input'
 import styled from 'styled-components'
 import { useState } from 'react'
-import {livros} from './dadosPesquisa'
+import { livros } from './dadosPesquisa'
 
 const PesquisaContainer = styled.section`
-        background-image: linear-gradient(90deg, #002F52 35%, #326589 165%);
-        color: #FFF;
-        text-align: center;
-        padding: 85px 0;
-        height: 270px;
-        width: 100%;
+    background-image: linear-gradient(90deg, #002F52 35%, #326589 165%);
+    color: #FFF;
+    text-align: center;
+    padding: 85px 0;
+    height: 270px;
+    width: 100%;
 `
 
 const Titulo = styled.h2`
-        color: #FFF;
-        font-size: 36px;
-        text-align: center;
-        width: 100%;
+    color: #FFF;
+    font-size: 36px;
+    text-align: center;
+    width: 100%;
 `
 
 const Subtitulo = styled.h3`
-        font-size: 16px;
-        font-weight: 500;
-        margin-bottom: 40px;
+    font-size: 16px;
+    font-weight: 500;
+    margin-bottom: 40px;
+`
+
+const Resultado = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin-bottom: 20px;
+    cursor: pointer;
+    p {
+        width: 200px;
+    }
+    img {
+        width: 100px;
+    }
+    &:hover {
+        border: 1px solid white;
+    }
 `
 
 function Pesquisa() {
-    const [livrosPesquisados, setLivrosPesquisados] = useState([])
+    const [ livrosPesquisados, setLivrosPesquisados ] = useState([])
 
-    return(
+    return (
         <PesquisaContainer>
             <Titulo>Já sabe por onde começar?</Titulo>
             <Subtitulo>Encontre seu livro em nossa estante.</Subtitulo>
@@ -36,11 +53,16 @@ function Pesquisa() {
                 placeholder="Escreva sua próxima leitura"
                 onBlur={evento => {
                     const textoDigitado = evento.target.value
-                    const resultadoPesquisa = livros.filter( livro => livro.nome.includes(textoDigitado))
+                    const resultadoPesquisa = livros.filter( livro => livro.nome.includes(textoDigitado) )
                     setLivrosPesquisados(resultadoPesquisa)
                 }}
             />
-        
+            { livrosPesquisados.map( livro => (
+                <Resultado>
+                    <img src={livro.src}/>
+                    <p>{livro.nome}</p>
+                </Resultado>
+            )) }
         </PesquisaContainer>
     )
 }
