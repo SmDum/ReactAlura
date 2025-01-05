e4efaf03f254a856ac65f7c48808c48e3ff4f91b
SmDum <samueldemoraesdelgado2005@gmail.com>
2025-01-05 10:42:40 -0300

    Exercícios

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import Header from './componentes/Header';
 import styled from 'styled-components';
 import Pesquisa from './componentes/Pesquisa'
+import UltimosLancamentos from './componentes/UltimosLancamentos';
 
 const AppContainer = styled.div`
       width: 100vw;
       height: 100vh;
-      background-image: linear-gradient(90deg, #002F52 35%, #326589);
+      background-image: linear-gradient(90deg, #002F52 35%, #326589 165%);
 `
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
     <AppContainer>
       <Header />
       <Pesquisa />
+      <UltimosLancamentos />
     </AppContainer>
   );
 }
