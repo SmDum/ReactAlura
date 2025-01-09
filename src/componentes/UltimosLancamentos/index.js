7db548a3233e68a56f0cef5c19224f0b18166ffc
SmDum <samueldemoraesdelgado2005@gmail.com>
2025-01-09 07:42:19 -0300

    Exercícios

--- src/componentes/UltimosLancamentos/index.js
+++ src/componentes/UltimosLancamentos/index.js
@@ -1,4 +1,5 @@
 import { livros } from './dadosUltimosLancamentos'
+import { Titulo } from '../Titulo'
 import styled from 'styled-components'
 
 const UltimosLancamentosContainer = styled.section`
@@ -6,16 +7,6 @@ const UltimosLancamentosContainer = styled.section`
     padding-bottom: 20px;
     display: flex;
     flex-direction: column;
-`
-
-const Titulo = styled.h2`
-    width: 100%;
-    padding: 30px 0;
-    background-color: #FFF;
-    color: #EB9B00;
-    font-size: 36px;
-    text-align: center;
-    margin: 0;
 `
 
 const NovosLivrosContainer = styled.div`
@@ -30,11 +21,15 @@ function UltimosLancamentos() {
     return (
         <UltimosLancamentosContainer>
 
-            <Titulo>ÚLTIMOS LANÇAMENTOS</Titulo>
+            <Titulo
+                cor="#EB9B00"
+                tamanhoFonte="36px">
+                ÚLTIMOS LANÇAMENTOS</Titulo>
+
             <NovosLivrosContainer>
-            {livros.map(livro => (
-                <img src={livro.src} />
-            ))}
+                {livros.map(livro => (
+                    <img src={livro.src} />
+                ))}
             </NovosLivrosContainer>
 
         </UltimosLancamentosContainer>
